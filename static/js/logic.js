301307e92a1418216a118080479c8bdadb14b1ec
amc1119 <alexis@Alexiss-MBP.lan>
2024-02-26 14:18:05 -0500

    Added earthquake data markers, reflecting the magnitude by size and depth.

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,12 +1,8 @@
  // Creating the map object
 let myMap = L.map("map", {
-    center: [0, 0],
+    center: [40, -90],
     zoom: 3
 });
-//  // Create the tile layer that will be the background of our map.
-//   let streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//   }).addTo(myMap);
 
 // Adding the OpenTopoMap tile layer
 L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
@@ -20,38 +16,105 @@ let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.ge
 // Get the data with d3.
 d3.json(url).then(function(data) {
 
-    features = data.features;
+    // features = data.features;
 
-    // Render all earthquake data
-    let allData = features.length;
+  function styleInfo(feature){
+    return{
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: getColor(feature.geometry.coordinates[2]),
+      color: '#000000',
+      radius: markerSize(feature.properties.mag),
+      stroke: true,
+      weight: 0.5
+    }
+  }
 
     // Define markerSize() function that will give each earthquakee a different radius based on its size.
-    function markerSize(mag) {
-        return Math.sqrt(mag) * 50;
+    function markerSize(magnitude) {
+      if (magnitude === 0) {
+        return 1;
+      }
+        return magnitude * 4;
     }
-  
-    // Loop through the data.
-    for (let i = 0; i < allData; i++) {
-  
-      // Set the earthquake coordinate data property to a variable.
-      let earthquake = features[i].geometry;
-      
-      // Set the earthquake size property to a variable.
-      let properites = features[i].properties;
-  
-        if(earthquake){
-        // L.marker([earthquake.coordinates[1], earthquake.coordinates[0]]).addTo(myMap);
-        L.circle([earthquake.coordinates[1], earthquake.coordinates[0]], {
-            fillOpacity: 0.75,
-            color: "white",
-            fillColor: "purple",
-            // Setting our circle's radius to equal the output of our markerSize() function:
-            // This will make our marker's size proportionate to its size.
-            radius: markerSize(properites[i].mag)
-          }).bindPopup(`<h1>${properites[i].title}</h1> <hr> <h3>Magnitude: ${properites[i].mag.toLocaleString()}</h3>`).addTo(myMap);
-        }
+    function getColor(depth) {
+      switch (true) {
+        case depth > 90:
+          return "#ea2c2c";
+        case depth > 70:
+          return "#ea822c";
+        case depth > 50:
+          return "#ee9c00";
+        case depth > 30:
+          return "#eecc00";
+        case depth > 10:
+          return "#d4ee00";
+        default:
+          return "#98ee00";
+      }
     }
-  
+
+  L.geoJson(data, {
+    pointToLayer: function(feature, latlng){
+      return L.circleMarker(latlng)
+    },
+    // We set the style for each circleMarker using our styleInfo function.
+    style: styleInfo,
+    // We create a popup for each marker to display the magnitude and location of the earthquake after the marker has been created and styled
+    onEachFeature: function (feature, layer) {
+      layer.bindPopup(
+        "Magnitude: "
+        + feature.properties.mag
+        + "<br>Depth: "
+        + feature.geometry.coordinates[2]
+        + "<br>Location: "
+        + feature.properties.place
+      );
+    }
+  }).addTo(myMap);
+
+  // Add the legend after the GeoJSON layer is added
+  let legend = L.control({
+    position: "bottomright"
   });
+
+  legend.onAdd = function (map) {
+    let div = L.DomUtil.create("div", "info legend");
+    let grades = [-10, 10, 30, 50, 70, 90];
+    let colors = ["#98ee00", "#d4ee00", "#eecc00", "#ee9c00", "#ea822c", "#ea2c2c"];
+
+    for (let i = 0; i < grades.length; i++) {
+      div.innerHTML +='<i style="background:' + colors[i] + '"></i> ' +
+      grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+  };
+
+  legend.addTo(myMap);
+
+  // Create the legend HTML
+  // let legendHtml = '<div id="legend" class="legend">';
+  // let grades = [-10, 10, 30, 50, 70, 90];
+  // let colors = ["#98ee00", "#d4ee00", "#eecc00", "#ee9c00", "#ea822c", "#ea2c2c"];
+
+  // for (let i = 0; i < grades.length; i++) {
+  //   legendHtml +=
+  //     '<div><i style="background:' + colors[i] + '"></i></div>' +
+  //     '<span class="legend-label">' + grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+") + '</span>';
+  // }
+
+  // legendHtml += '</div>';
+
+  // // Append legend HTML to the map container
+  // let legendContainer = L.DomUtil.create('div', 'legend-container');
+  // legendContainer.innerHTML = legendHtml;
+  // document.getElementById('map').appendChild(legendContainer);
+
+
+});
+  
+    
+
 
   
